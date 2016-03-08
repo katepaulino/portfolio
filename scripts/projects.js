ed1e30112fc99567ea6d7ef4cb3a631109f01f91
Kate Lucas <katherinelucas@gmail.com>
2016-03-08 09:02:26 -0800

    edited project links

--- scripts/projects.js
+++ scripts/projects.js
@@ -3,7 +3,7 @@ var projectData = [
     title:       'Cookie Stand',
     category:    'cookies',
     author:      'Katherine Paulino Lucas',
-    authorUrl:   'https://github.com/katepaulino',
+    authorUrl:   'http://katepaulino.github.io/cookie-stand',
     publishedOn: '2016-02-05',
     body:        '<p><img src="img/cookiestand.png" class="screenshot"></p>'
   },
@@ -11,7 +11,7 @@ var projectData = [
     title:       'Twimpact',
     category:    'nonprofit',
     author:      'Katherine Paulino Lucas, Daniel Lee, Sarah Wong',
-    authorUrl:   'https://github.com/katepaulino/twimpact',
+    authorUrl:   'http://katepaulino.github.io/twimpact/',
     publishedOn: '2016-02-28',
     body:        '<p><img src="img/twimpact.png" class="screenshot"></p>'
   },
@@ -19,7 +19,7 @@ var projectData = [
     title:       'Market Analysis App',
     category:    'marketing',
     author:      'Katherine Paulino Lucas',
-    authorUrl:   'https://github.com/katepaulino',
+    authorUrl:   'https://github.com/katepaulino/market-analysis-app',
     publishedOn: '2016-02-15',
     body:        '<p><img src="img/maa.png" class="screenshot"></p>'
   },
