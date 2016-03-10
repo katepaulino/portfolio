4e91d8c44eca743970d2845d1c009a7783cfcda7
Kate Lucas <katherinelucas@gmail.com>
2016-03-10 13:41:20 -0800

    RWD working

--- scripts/app.js
+++ scripts/app.js
@@ -10,22 +10,9 @@ function Project (opts) {
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
-  $newProject.removeClass('template');
-  if (!this.publishedOn) {
-    $newProject.addClass('draft');
-  }
-
-  $newProject.data('category', this.category);
-  $newProject.find('.byline a').html(this.author);
-  $newProject.find('.byline a').attr('href', this.authorUrl);
-  $newProject.find('h1').html(this.title);
-  $newProject.find('.article-body').html(this.body);
-  $newProject.find('time[pubdate]').attr('datetime', this.publishedOn);
-  $newProject.find('time[pubdate]').attr('title', this.publishedOn);
-  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
-  return $newProject;
+  var source = $('#project-template').html();
+  var template = Handlebars.compile(source);
+  return template(this);
 };
 
 projectData.sort(function(a,b) {
