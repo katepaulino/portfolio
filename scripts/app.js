628e95c09577f6806fb9279ece59e863e9bd3a21
Kate Lucas <katherinelucas@gmail.com>
2016-03-15 23:17:10 -0700

    IIFE created for js files, no for loops, still trying to figure out how to incorporate .map and .reduce

--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,43 @@
 (function(module) {
 
-function Project(opts) {
-  'use strict';
-  this.author = opts.author;
-  this.title = opts.title;
-  this.category = opts.category;
-  this.authorUrl = opts.authorUrl;
-  this.publishedOn = opts.publishedOn;
-  this.body = opts.body;
-}
-
-Project.all = [];
-
-Project.prototype.toHtml = function() {
-  var template = Handlebars.compile($('#project-template').text());
-  return template(this);
-};
-
-Project.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-  rawData.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
-
-
-Project.fetchAll = function(next) {
-  if (localStorage.rawData) {
-    Project.loadAll(localStorage.rawData);
-    next();
-  } else {
-    $.getJSON('/data/projects.json', function(rawData){
-      Project.loadAll(rawData);
-      localStorage.setItem('rawData', JSON.stringify(rawData));
-      ProjectView.initIndexPage();
-    });
+  function Project(opts) {
+    this.author = opts.author;
+    this.title = opts.title;
+    this.category = opts.category;
+    this.authorUrl = opts.authorUrl;
+    this.publishedOn = opts.publishedOn;
+    this.body = opts.body;
   }
-};
+
+  Project.all = [];
+
+  Project.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#project-template').text());
+    return template(this);
+  };
+
+  Project.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+    rawData.forEach(function(ele) {
+      Project.all.push(new Project(ele));
+    });
+  };
+
+
+  Project.fetchAll = function(next) {
+    if (localStorage.rawData) {
+      Project.loadAll(JSON.parse(localStorage.rawData));
+      next();
+    } else {
+      $.getJSON('/data/projects.json', function(rawData){
+        Project.loadAll(rawData);
+        localStorage.setItem('rawData', JSON.stringify(rawData));
+        next();
+      });
+    }
+  };
 
   module.Project = Project;
-};(window);
+})(window);
